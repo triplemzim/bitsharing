88d9d0a59608ea850393b858d81c0284c0c72aab
triplemzim <triplemzim@gmail.com>
2024-01-08 02:31:27 +0800

    react + tailwindcss + postcss + autoprefixer + vite boilerplate

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+function App() {
+
+  return (
+    <>
+      <div className="w-full flex justify-items-center justify-center">
+        <h2 className="flex justify-center w-1/4 text-3xl font-bold bg-slate-400">
+          Home
+        </h2>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/*eslint-env node*/
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./src/**/*.{js,jsx,ts,tsx}",
+      './app/**/*.{js,jsx}',
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
