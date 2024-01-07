488e2b072f0a7896db76fca494eb788dddd32e9b
triplemzim <triplemzim@gmail.com>
2024-01-08 03:06:37 +0800

    shadcn integrated

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
+import { useState } from "react"
+import { Button } from "@/components/ui/button"
+
 
 function App() {
+  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className="w-full flex justify-items-center justify-center">
-        <h2 className="flex justify-center w-1/4 text-3xl font-bold bg-slate-400">
-          Home
+      <div className="w-full mt-4 flex justify-items-center justify-center">
+        <div className="self-center space-y-4">
+        <h2 className="p-4 text-3xl font-bold rounded-lg bg-slate-400">
+          Count: {count}
         </h2>
+        <Button onClick={() => setCount(count + 1)}>Click me</Button>
+        </div>
       </div>
     </>
   )
